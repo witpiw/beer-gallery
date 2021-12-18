e026ffb9d88affe1699fe0154c2425f0f63e7bee
witpiw <piweckiwit@gmail.com>
2021-12-18 17:49:40 +0100

    added handling bad requests

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -12,30 +12,52 @@ import defaultImage from "../../images/beer-bottle.png";
 
 const Main = () => {
   const [beers, setBeers] = useState([]);
+  const [error, setError] = useState(false);
   const [page, setPage] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetchBeers(page);
-      setBeers(res);
+      try {
+        const res = await fetchBeers(page);
+        setBeers(res);
+        setError(false);
+      } catch {
+        setError(true);
+      }
     };
     fetchData();
   }, [page]);
 
+  const renderBeerCards = () => {
+    return beers.map(({ id, image_url, name, description }) => (
+      <BeerCard
+        description={description}
+        name={name}
+        key={id}
+        id={id}
+        img={image_url || defaultImage}
+      />
+    ));
+  };
+
+  const renderContent = () => {
+    if (!error && beers.length !== 0) {
+      return renderBeerCards();
+    } else if (beers.length === 0 && !error) {
+      return <Loader />;
+    } else {
+      return (
+        <p>
+          Something went wrong :( <br />
+          Please try again later
+        </p>
+      );
+    }
+  };
+
   return (
     <>
-      <main>
-        {beers.length === 0 && <Loader />}
-        {beers.map(({ id, image_url, name, description }) => (
-          <BeerCard
-            description={description}
-            name={name}
-            key={id}
-            id={id}
-            img={image_url || defaultImage}
-          />
-        ))}
-      </main>
+      <main>{renderContent()}</main>
       <Pager
         resetBeers={() => setBeers([])}
         changePage={(val) => setPage(val)}
